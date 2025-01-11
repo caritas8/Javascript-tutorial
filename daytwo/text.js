be9d2d640c28136f256e1f01843d42424fa62cff
ezeakaluecari@gmail.com <ezeakaluejudejnr@gmail.com>
2025-01-10 19:51:51 -0800

    day two

---
+++ daytwo/text.js
@@ -0,0 +1,14 @@
+function changeText () {
+    var parent = document.getElementById("main");
+    var child = parent.lastElementChild;
+
+   
+    
+     /* we do not need to worry about white spaces here. Last: gets the last paragraph. First: gest the 1st paragraph
+     
+   child.style.color = "blue";
+   //var child = parent.childNodes [7];
+   /*builds a node list of all the nodes under our parent
+    We target the second paragraph here. Note that child node accomodates both the white spaces. Element accomadates only the para.
+    */
+}
